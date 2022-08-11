5eb4bb3357d3ce6a52012ecdd83a950d9da30107
hien nguyen <thuhiennguyen198@gmail.com>
2022-08-10 20:51:07 -0400

    add actions sttorejs reducers

---
+++ frontend/actions/players_action.js
@@ -0,0 +1,6 @@
+export const SET_PLAYER_INFO = "SET_PLAYER_INFO";
+
+export const setPlayerInfo = (playerInfo) => ({
+  type: SET_PLAYER_INFO,
+  playerInfo,
+});
---
+++ frontend/src/reducers/root_reducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import players from "./players_reducer";
+const rootReducer = combineReducers({
+  players
+});
+
+export default rootReducer;
